cface758709d675e0f1e8ec84cb9879f491d3e56
ewilly <ewilly@neuf.fr>
2019-11-18 10:23:03 +0100

    Update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,7 @@
 #
 
 # Package dependencies
-if [ "$(lsb_release --codename --short)" != "jessie" ]; then
-	PKG_DEPENDENCIES="php-zip php-curl php-gd"
-else
-	PKG_DEPENDENCIES="php5-curl php5-gd"
-fi
+PKG_DEPENDENCIES="php-zip php-curl php-gd"
 
 # Check if directory/file already exists (path in argument)
 myynh_check_path () {
@@ -48,4 +44,36 @@ myynh_set_permissions () {
 	chown -R "$app": "$final_path/private"
 	chown -R "$app": "$data_path"
 	chown root: "$data_path"
+}
+
+#Change --data to --data-urlencode
+myynh_local_curl () {
+    # Define url of page to curl
+    local local_page=$(ynh_normalize_url_path $1)
+    local full_path=$path_url$local_page
+
+    if [ "${path_url}" == "/" ]; then
+        full_path=$local_page
+    fi
+
+    local full_page_url=https://localhost$full_path
+
+    # Concatenate all other arguments with '&' to prepare POST data
+    local POST_data=""
+    local arg=""
+    for arg in "${@:2}"
+    do
+        POST_data="${POST_data}${arg}&"
+    done
+    if [ -n "$POST_data" ]
+    then
+        # Add --data-urlencode arg and remove the last character, which is an unecessary '&'
+        POST_data="--data-urlencode ${POST_data::-1}"
+    fi
+
+    # Wait untils nginx has fully reloaded (avoid curl fail with http2)
+    sleep 2
+
+    # Curl the URL
+    curl --silent --show-error -kL -H "Host: $domain" --resolve $domain:443:127.0.0.1 $POST_data "$full_page_url"
 }
